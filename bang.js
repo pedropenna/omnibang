b7cbc2d1e58aac49af5d95f37ef8189d2f313d31
Pedro Penna <ppenna@thoughtworks.com>
2019-05-29 00:37:09 -0300

    filter out the bang
    add more bangs
    
    fix manifest.json

--- bang.js
+++ bang.js
@@ -1,8 +1,18 @@
+chrome.omnibox.setDefaultSuggestion({
+  description: '!!!'
+});
+
 chrome.omnibox.onInputEntered.addListener(function (text) {
   var bangs = {
     '!s': 'https://open.spotify.com/search/results/',
     '!g': 'https://www.google.com/search?q=',
     '!d': 'https://duckduckgo.com/?q=',
+    '!m': 'https://www.google.com/maps?q=',
+    '!w': 'https://en.wikipedia.org/wiki/',
+    '!yt': 'https://www.youtube.com/results?search_query=',
+    '!gi': 'https://www.google.com/search?tbm=isch&tbs=imgo:1&q=',
+    '!ml': 'https://lista.mercadolivre.com.br/',
+    '!gt': 'https://translate.google.com/#auto/pt/',
   };
 
   var baseUrl;
@@ -13,6 +23,8 @@ chrome.omnibox.onInputEntered.addListener(function (text) {
   } else {
     baseUrl = bangs['!g'];
   }
+
+  text = text.split(' ').filter(s => !s.startsWith('!')).join(' ');
 
   chrome.tabs.query({
     'currentWindow': true,
