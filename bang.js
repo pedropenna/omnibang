35bfb8f46ab55c5d7baebee609a6ad7ad0fc2b5e
Pedro Penna <ppenna@thoughtworks.com>
2019-05-28 23:52:15 -0300

    get it to work, finally

--- bang.js
+++ bang.js
@@ -1,8 +1,70 @@
-
-browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
-  alert('input changed - ' + text);
+browser.omnibox.setDefaultSuggestion({
+  description: "Type the name of a CSS property"
 });
 
-browser.omnibox.onInputEntered.addListener((text, disposition) => {
-  alert('input entered - ' + text);
+/*
+Very short list of a few CSS properties.
+*/
+const props = [
+  "animation",
+  "background",
+  "border",
+  "box-shadow",
+  "color",
+  "display",
+  "flex",
+  "flex",
+  "float",
+  "font",
+  "grid",
+  "margin",
+  "opacity",
+  "overflow",
+  "padding",
+  "position",
+  "transform",
+  "transition"
+];
+
+const baseURL = "https://developer.mozilla.org/en-US/docs/Web/CSS/";
+
+/*
+Return an array of SuggestResult objects,
+one for each CSS property that matches the user's input.
+*/
+function getMatchingProperties(input) {
+  var result = [];
+  for (prop of props) {
+    if (prop.indexOf(input) === 0) {
+      console.log(prop);
+      let suggestion = {
+        content: baseURL + prop,
+        description: prop
+      }
+      result.push(suggestion);
+    } else {
+      if (result.length != 0) {
+        return result;
+      }
+    }
+  }
+  return result;
+}
+
+browser.omnibox.onInputChanged.addListener((input, suggest) => {
+  suggest(getMatchingProperties(input));
+});
+
+browser.omnibox.onInputEntered.addListener((url, disposition) => {
+  switch (disposition) {
+    case "currentTab":
+      browser.tabs.update({url});
+      break;
+    case "newForegroundTab":
+      browser.tabs.create({url});
+      break;
+    case "newBackgroundTab":
+      browser.tabs.create({url, active: false});
+      break;
+  }
 });
